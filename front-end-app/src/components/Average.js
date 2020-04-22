fb9f3b7d5dc92380e2e7508b602fc1e12b5cb9a9
Garen Dadourian <garendadourian@Garens-MBP.home>
2020-04-22 14:55:03 -0400

    round average to whole number

--- front-end-app/src/components/Average.js
+++ front-end-app/src/components/Average.js
@@ -4,8 +4,8 @@ import './Average.css';
 export const Average = ({ grades }) => {
     let gradeSum = 0;
     grades.forEach(grade => gradeSum += grade.score_received)
-    const gradeAverage = gradeSum / grades.length
-    
+    const gradeAverage = Math.round(gradeSum / grades.length)
+
     return (
         <p className='average'>
             <span>Average:</span>

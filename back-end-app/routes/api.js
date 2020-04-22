199db9abb424a35b7cf3736c5e5939910cddb492
Garen Dadourian <garendadourian@Garens-MBP.home>
2020-04-22 11:40:30 -0400

    add grades hardcoded api

--- back-end-app/routes/api.js
+++ back-end-app/routes/api.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var studentsData = require('../views/students.json')
+var gradesData = require('../views/grades.json')
 
-/* GET users listing. */
+/* GET students listing. */
 router.get('/students', function(req, res, next) {
   res.json(studentsData);
+});
+
+/* GET grades listing. */
+router.get('/grades', function(req, res, next) {
+  res.json(gradesData);
 });
 
 module.exports = router;

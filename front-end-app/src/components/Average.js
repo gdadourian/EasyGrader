3e8142f1702b7d6cd8f0cf5ebe0ccd1824a098fa
Garen Dadourian <garendadourian@Garens-MacBook-Pro.local>
2020-04-29 16:45:50 -0400

    fix average bug

--- front-end-app/src/components/Average.js
+++ front-end-app/src/components/Average.js
@@ -2,9 +2,13 @@ import React from 'react'
 import './Average.css';
 
 export const Average = ({ grades }) => {
-    let gradeSum = 0;
-    grades.forEach(grade => gradeSum += grade.score_received)
-    const gradeAverage = Math.round(gradeSum / grades.length)
+    let gradeReceivedSum = 0;
+    let gradePossibleSum = 0;
+    grades.forEach(grade => {
+        gradeReceivedSum += grade.score_received
+        gradePossibleSum += grade.score_possible
+    })
+    const gradeAverage = Math.round(gradeReceivedSum / gradePossibleSum * 100)
 
     return (
         <p className='average'>

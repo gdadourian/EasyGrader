61eee5584e39948cb2fef1e543847613e7c28d4b
Garen Dadourian <garendadourian@Garens-MBP.home>
2020-05-04 15:51:12 -0400

    add mongo test config

--- back-end-app/app.test.js
+++ back-end-app/app.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("./app");
+const { MongoClient } = require('mongodb');
 
 describe("Test the root path", () => {
   test("It should responsd to the GET method", async () => {
@@ -8,38 +9,51 @@ describe("Test the root path", () => {
   });
 });
 
-// describe('Test students api', () => {
-//     test('GET /students should return 200', async () => {
-//         const response = await request(app).get('/api/students');
-//         expect(response.statusCode).toBe(200);
-//     })
-//     test('students have id, first_name and last_name', async () => {
-//         const response = await request(app).get('/api/students');
-//         response.body.forEach(student =>
-//             expect(student).toEqual(expect.objectContaining({
-//                 id: expect.any(Number),
-//                 first_name: expect.any(String),
-//                 last_name: expect.any(String),
-//             }))
-//         )
-//     })
-// })
+describe('Test api', () => {
+    let connection;
+    let db;
 
-// describe('Test grades api', () => {
-//     test('GET /grades should return 200', async () => {
-//         const response = await request(app).get('/api/grades');
-//         expect(response.statusCode).toBe(200);
-//     })
-//     test('grades have id, student_id, assignment, score_received and score_possible', async () => {
-//         const response = await request(app).get('/api/grades');
-//         response.body.forEach(student =>
-//             expect(student).toEqual(expect.objectContaining({
-//                 id: expect.any(Number),
-//                 student_id: expect.any(Number),
-//                 assignment: expect.any(String),
-//                 score_received: expect.any(Number),
-//                 score_possible: expect.any(Number),
-//             }))
-//         )
-//     })
-// })
+    beforeAll(async () => {
+      connection = await MongoClient.connect(process.env.MONGODB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      db = await connection.db();
+    });
+
+    afterAll(async () => {
+      await connection.close();
+    });
+
+    test('GET /students should return 200', async () => {
+        const response = await request(app).get('/api/students');
+        expect(response.statusCode).toBe(200);
+    })
+    test('students have id, first_name and last_name', async () => {
+        const response = await request(app).get('/api/students');
+        response.body.forEach(student =>
+            expect(student).toEqual(expect.objectContaining({
+                id: expect.any(Number),
+                first_name: expect.any(String),
+                last_name: expect.any(String),
+            }))
+        )
+    })
+
+    test('GET /grades should return 200', async () => {
+        const response = await request(app).get('/api/grades');
+        expect(response.statusCode).toBe(200);
+    })
+    test('grades have id, student_id, assignment, score_received and score_possible', async () => {
+        const response = await request(app).get('/api/grades');
+        response.body.forEach(student =>
+            expect(student).toEqual(expect.objectContaining({
+                id: expect.any(Number),
+                student_id: expect.any(Number),
+                assignment: expect.any(String),
+                score_received: expect.any(Number),
+                score_possible: expect.any(Number),
+            }))
+        )
+    })
+})
